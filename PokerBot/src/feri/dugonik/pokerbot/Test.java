8dfbf1f852fc3e97aaaa34957272e2d50c41024d
Jani Dugonik <jani.dugonik@gmail.com>
2012-05-22 12:51:30 +0200

    Psevdokodi

---
+++ PokerBot/src/feri/dugonik/pokerbot/Test.java
@@ -0,0 +1,46 @@
+package feri.dugonik.pokerbot;
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+
+		/*
+		 * 
+		 * after the flop - trije koraki:
+		 * 1. hand strength, hand potential
+		 * 2. pravila za stavljanje
+		 * 3. random številka med 0 in 1 in jo uporabimo da izberemo fold, call, raise
+		 * 
+		 */
+		
+		
+		
+		
+
+	}
+	
+	/*psevodkod:
+	HandStrength(ourcards, boardcards)
+	{
+		ahead = tied = behind = 0
+		ourrank = Rank(ourcards, boardcards)
+		
+		foreach oppcards
+		{
+			opprank = Rank(oppcards, boardcards)
+			if (ourrank > opprank) ahead += 1
+			else if (ourrank == opprank) tied += 1
+			else behind += 1
+		}
+		
+		handstrength = (ahead+tied/2) / (ahead+tied+behind)
+		return handstrength
+	}*/
+	
+	//psevdokod:
+	
+}
